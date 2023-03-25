00ee0e056a93f68b70e0eff8a1b51e4a6276d9c3
f_nosrati <f_nosrati@SHL-62>
2023-03-25 16:24:22 +0330

    Add fake data in Mock date

--- src/UnitTestUsingXUnit.DataAccess/Dtos/CreateProduct.cs
+++ src/UnitTestUsingXUnit.DataAccess/Dtos/CreateProduct.cs
@@ -2,5 +2,12 @@
 {
     public class CreateProduct
     {
+        public string ProductName { get; set; }
+
+        public string ProductTitle { get; set; }
+
+        public string ProductType { get; set; }
+
+        public float ProductPrice { get; set; }
     }
 }
