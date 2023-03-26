5b6f5cc193c0d6fbf9827e69fd5a695b50cdf23e
f_nosrati <f_nosrati@SHL-62>
2023-03-26 17:23:24 +0330

    Add CreateProduct

--- src/UnitTestUsingXUnit.Test/ProductsControllerTest.cs
+++ src/UnitTestUsingXUnit.Test/ProductsControllerTest.cs
@@ -4,6 +4,12 @@ using System.Text;
 using UnitTestUsingXUnit.Test.MockDatas;
 using System.Net;
 using FluentAssertions;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Http;
+using System.Net.Http;
+using Microsoft.Extensions.Options;
+using System.Net.Http.Headers;
+using System;
 
 namespace UnitTestUsingXUnit.Test
 {
@@ -22,38 +28,64 @@ namespace UnitTestUsingXUnit.Test
         #endregion Ctor
 
         [Fact]
-        public async Task When_ValidCreateProductInCreateProductAsyncthene_ShouldBeCreated()
+        public async Task When_ValidCreateProductInCreateProductAsyncthene_MustBeCreated()
         {
             var createProduct = ProductMockData.ValidCreateProduct;
 
             string uri = $"/api/v1/product";
+            
+            var httpContent = CreateHttpContent(createProduct);
 
-            var bodyStr = JsonSerializer.Serialize(createProduct);
-            var result = new StringContent(bodyStr, Encoding.UTF8, MediaTypeNames.Application.Json);
-
-            var response = await _httpClient.PostAsync(uri, result);
+            var response = await _httpClient.PostAsync(uri, httpContent);
 
             response.IsSuccessStatusCode.Should().BeTrue();
             response.StatusCode.Should().Be(HttpStatusCode.Created);
+
+            // your must segregate to helper or common class for use global test project
+            HttpContent CreateHttpContent<T>(T model)
+            {
+                var bodyStr = JsonSerializer.Serialize(model);
+
+                var result = new StringContent(bodyStr, Encoding.UTF8, MediaTypeNames.Application.Json);
+
+                return result;
+            }
         }
 
 
         [Fact]
-        public async Task When_InValidCreateProductInCreateProductAsyncthene_ShouldBeCreated()
+        public async void When_InValidCreateProductInCreateProductAsyncthene_MustBeThrowExceptionMessageProductNameIsExist()
         {
+            // Arrange
             var createProduct = ProductMockData.InValidCreateProduct;
 
             string uri = $"/api/v1/product";
 
-            var bodyStr = JsonSerializer.Serialize(createProduct);
-            var result = new StringContent(bodyStr, Encoding.UTF8, MediaTypeNames.Application.Json);
+            var httpContent = CreateHttpContent(createProduct);
 
-            var response = await _httpClient.PostAsync(uri, result);
+            // Act
+            var response = await _httpClient.PostAsync(uri, httpContent);
 
+            // Assert
             response.IsSuccessStatusCode.Should().BeFalse();
             response.StatusCode.Should().Be(HttpStatusCode.InternalServerError);
 
+            //you can get exception message and compare here.
+            ///exceptionMessage == "Product Name Is Exist";
+
+            // your must segregate to helper or common class for use global test project
+            HttpContent CreateHttpContent<T>(T model)
+            {
+                var bodyStr = JsonSerializer.Serialize(model);
+
+                var result = new StringContent(bodyStr, Encoding.UTF8, MediaTypeNames.Application.Json);
+
+                return result;
+            }
         }
+
+
+
     }
 
 }
