e4334e22ad93ab8af60efcedd61ddf9fc185855a
f_nosrati <f_nosrati@SHL-62>
2023-03-26 15:30:40 +0330

    Fix exception test messages

--- src/UnitTestUsingXUnit.Test/ProductsControllerTest.cs
+++ src/UnitTestUsingXUnit.Test/ProductsControllerTest.cs
@@ -5,10 +5,15 @@ using System.Text;
 using System;
 using UnitTestUsingXUnit.Test.MockDatas;
 using System.Net;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using System.Net.Http.Json;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+using System.Net.Mail;
 
 namespace UnitTestUsingXUnit.Test
 {
-    public class ProductsControllerTest: IClassFixture<StartupFactory>
+    public class ProductsControllerTest : IClassFixture<StartupFactory>
     {
         #region Fields
         private readonly HttpClient _httpClient;
@@ -23,9 +28,9 @@ namespace UnitTestUsingXUnit.Test
         #endregion Ctor
 
         [Fact]
-        public async Task When_ValidCreateProductInCreateProductAsyncthene_CreatedInDataBase()
+        public async Task When_ValidCreateProductInCreateProductAsyncthene_ShouldBeCreated()
         {
-            var createProduct = ProductMockData.DynamicCreateProduct;
+            var createProduct = ProductMockData.ValidCreateProduct;
 
             string uri = $"/api/v1/product";
 
@@ -34,10 +39,83 @@ namespace UnitTestUsingXUnit.Test
 
             var response = await _httpClient.PostAsync(uri, result);
 
-            response.EnsureSuccessStatusCode();
+            response.IsSuccessStatusCode.Should().BeTrue();
+            response.StatusCode.Should().Be(HttpStatusCode.Created);
+        }
 
-            //response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+
+        [Fact]
+        public async Task When_InValidCreateProductInCreateProductAsyncthene_ShouldBeCreated()
+        {
+            var createProduct = ProductMockData.InValidCreateProduct;
+
+            string uri = $"/api/v1/product";
+
+            var bodyStr = JsonSerializer.Serialize(createProduct);
+            var result = new StringContent(bodyStr, Encoding.UTF8, MediaTypeNames.Application.Json);
+
+            var response = await _httpClient.PostAsync(uri, result);
+
+            response.IsSuccessStatusCode.Should().BeFalse();
+            response.StatusCode.Should().Be(HttpStatusCode.InternalServerError);
+
+            string exceptionMessage = response.ReasonPhrase;
+
+            //exceptionMessage.Should().Be("Product Name Is Exist");
+
+            var erere = await response.Content.ReadFromJsonAsync<Exception>();
+
 
         }
+
     }
+    //    private HttpResponseMessage CreateResponse(HttpRequestMessage request, Exception ex)
+    //    {
+    //        string message = ex.Message;
+
+    //        HttpStatusCode code = 0;
+
+    //        if (ex is KeyNotFoundException) code = HttpStatusCode.NotFound;
+    //        else if (ex is ArgumentException) code = HttpStatusCode.BadRequest;
+    //        else if (ex is InvalidOperationException) code = HttpStatusCode.BadRequest;
+    //        else if (ex is UnauthorizedAccessException) code = HttpStatusCode.Unauthorized;
+    //        else if (ex is HttpException)
+    //        {
+    //            // HttpExceptions are thrown when request between IdentityServer and the API server have failed.
+    //            // IdentityServer has generated an error, the API server received it and now it needs to relay it back to the client.
+    //            var httpException = (HttpException)ex;
+
+    //            code = (HttpStatusCode)httpException.GetHttpCode();
+    //            message = httpException.Message;
+    //        }
+    //        else
+    //        {
+    //            code = HttpStatusCode.InternalServerError;
+
+    //            // For security reasons, when an exception is not handled the system should return a general error, not exposing the real error information
+    //            // In development time, the programmer will need the details of the error, so this general message is disabled.
+    //#if DEBUG
+    //            message = ex.Message;
+    //#else
+    //            message = Errors.InternalServerError;
+    //#endif
+    //        }
+
+    //        // For some reason the request.CreateErrorResponse() method ignores the message given to it and parses its own message.
+    //        // The error response is constructed manually.
+    //        return CreateErrorResponse(request, code, message);
+    //    }
+
+    //    private HttpResponseMessage CreateErrorResponse(HttpRequestMessage request, HttpStatusCode code, string message)
+    //    {
+    //        var content = new { Message = message };
+
+    //        return new HttpResponseMessage(code)
+    //        {
+    //            ReasonPhrase = message,
+    //            RequestMessage = request,
+    //            Content = new ObjectContent(content.GetType(), content, new JsonMediaTypeFormatter())
+    //        };
+    //    }
+
 }
