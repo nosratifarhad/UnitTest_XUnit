8418a800922be1ec226218c721a687c9786ff2b2
f_nosrati <f_nosrati@SHL-62>
2023-04-12 21:04:27 +0330

    Add Mock data UpdateProductInputModel

--- src/tests/ECommerce.UnitTest/MockDatas/ProductMockData.cs
+++ src/tests/ECommerce.UnitTest/MockDatas/ProductMockData.cs
@@ -6,6 +6,11 @@ namespace ECommerce.UnitTest.MockDatas
 {
     public static class ProductMockData
     {
+        public const int Zero = 0;
+
+        public const int NegativeOne =-1;
+
+        #region [ CreateProductInputModel ]
         public static CreateProductInputModel ProductNameIsNullInC1reateProductInputModel()
             => new Faker<CreateProductInputModel>()
               .RuleFor(bp => bp.ProductName, f => null)
@@ -47,5 +52,89 @@ namespace ECommerce.UnitTest.MockDatas
               .RuleFor(bp => bp.IsFreeDelivery, f => f.Random.Bool())
               .RuleFor(bp => bp.IsExisting, f => f.Random.Bool())
               .RuleFor(bp => bp.Weight, f => f.Random.Number());
+
+
+        #endregion [ CreateProductInputModel ]
+
+        #region [ UpdateProductInputModel ]
+
+        public static UpdateProductInputModel ValidUpdateProductInputModel()
+            => new Faker<UpdateProductInputModel>()
+              .RuleFor(bp => bp.ProductId, f => f.Random.Number(1,50))
+              .RuleFor(bp => bp.ProductName, f => f.Name.FirstName())
+              .RuleFor(bp => bp.ProductTitle, f => f.Name.JobTitle())
+              .RuleFor(bp => bp.ProductDescription, f => f.Name.JobDescriptor())
+              .RuleFor(bp => bp.ProductCategory, f => f.Random.Enum<ProductCategory>())
+              .RuleFor(bp => bp.MainImageName, f => f.Name.FullName())
+              .RuleFor(bp => bp.MainImageTitle, f => f.Name.FullName())
+              .RuleFor(bp => bp.MainImageUri, f => f.Name.FullName())
+              .RuleFor(bp => bp.Color, f => f.Random.Enum<ProductColor>())
+              .RuleFor(bp => bp.IsFreeDelivery, f => f.Random.Bool())
+              .RuleFor(bp => bp.IsExisting, f => f.Random.Bool())
+              .RuleFor(bp => bp.Weight, f => f.Random.Number());
+
+        public static UpdateProductInputModel ZeroProductIdInUpdateProductInputModel()
+            => new Faker<UpdateProductInputModel>()
+              .RuleFor(bp => bp.ProductId, f => Zero)
+              .RuleFor(bp => bp.ProductName, f => f.Name.FirstName())
+              .RuleFor(bp => bp.ProductTitle, f => f.Name.JobTitle())
+              .RuleFor(bp => bp.ProductDescription, f => f.Name.JobDescriptor())
+              .RuleFor(bp => bp.ProductCategory, f => f.Random.Enum<ProductCategory>())
+              .RuleFor(bp => bp.MainImageName, f => f.Name.FullName())
+              .RuleFor(bp => bp.MainImageTitle, f => f.Name.FullName())
+              .RuleFor(bp => bp.MainImageUri, f => f.Name.FullName())
+              .RuleFor(bp => bp.Color, f => f.Random.Enum<ProductColor>())
+              .RuleFor(bp => bp.IsFreeDelivery, f => f.Random.Bool())
+              .RuleFor(bp => bp.IsExisting, f => f.Random.Bool())
+              .RuleFor(bp => bp.Weight, f => f.Random.Number());
+
+        public static UpdateProductInputModel NegativeOneProductIdInUpdateProductInputModel()
+            => new Faker<UpdateProductInputModel>()
+              .RuleFor(bp => bp.ProductId, f => NegativeOne)
+              .RuleFor(bp => bp.ProductName, f => f.Name.FirstName())
+              .RuleFor(bp => bp.ProductTitle, f => f.Name.JobTitle())
+              .RuleFor(bp => bp.ProductDescription, f => f.Name.JobDescriptor())
+              .RuleFor(bp => bp.ProductCategory, f => f.Random.Enum<ProductCategory>())
+              .RuleFor(bp => bp.MainImageName, f => f.Name.FullName())
+              .RuleFor(bp => bp.MainImageTitle, f => f.Name.FullName())
+              .RuleFor(bp => bp.MainImageUri, f => f.Name.FullName())
+              .RuleFor(bp => bp.Color, f => f.Random.Enum<ProductColor>())
+              .RuleFor(bp => bp.IsFreeDelivery, f => f.Random.Bool())
+              .RuleFor(bp => bp.IsExisting, f => f.Random.Bool())
+              .RuleFor(bp => bp.Weight, f => f.Random.Number());
+
+        public static UpdateProductInputModel ProductNameIsNullInUpdateProductInputModel()
+            => new Faker<UpdateProductInputModel>()
+              .RuleFor(bp => bp.ProductId, f => f.Random.Number(1, 50))
+              .RuleFor(bp => bp.ProductName, f => null)
+              .RuleFor(bp => bp.ProductTitle, f => f.Name.JobTitle())
+              .RuleFor(bp => bp.ProductDescription, f => f.Name.JobDescriptor())
+              .RuleFor(bp => bp.ProductCategory, f => f.Random.Enum<ProductCategory>())
+              .RuleFor(bp => bp.MainImageName, f => f.Name.FullName())
+              .RuleFor(bp => bp.MainImageTitle, f => f.Name.FullName())
+              .RuleFor(bp => bp.MainImageUri, f => f.Name.FullName())
+              .RuleFor(bp => bp.Color, f => f.Random.Enum<ProductColor>())
+              .RuleFor(bp => bp.IsFreeDelivery, f => f.Random.Bool())
+              .RuleFor(bp => bp.IsExisting, f => f.Random.Bool())
+              .RuleFor(bp => bp.Weight, f => f.Random.Number());
+
+        public static UpdateProductInputModel ProductTitleIsNullInUpdateProductInputModel()
+            => new Faker<UpdateProductInputModel>()
+              .RuleFor(bp => bp.ProductId, f => f.Random.Number(1, 50))
+              .RuleFor(bp => bp.ProductName, f => f.Name.FirstName())
+              .RuleFor(bp => bp.ProductTitle, f => null)
+              .RuleFor(bp => bp.ProductDescription, f => f.Name.JobDescriptor())
+              .RuleFor(bp => bp.ProductCategory, f => f.Random.Enum<ProductCategory>())
+              .RuleFor(bp => bp.MainImageName, f => f.Name.FullName())
+              .RuleFor(bp => bp.MainImageTitle, f => f.Name.FullName())
+              .RuleFor(bp => bp.MainImageUri, f => f.Name.FullName())
+              .RuleFor(bp => bp.Color, f => f.Random.Enum<ProductColor>())
+              .RuleFor(bp => bp.IsFreeDelivery, f => f.Random.Bool())
+              .RuleFor(bp => bp.IsExisting, f => f.Random.Bool())
+              .RuleFor(bp => bp.Weight, f => f.Random.Number());
+
+
+        #endregion [ UpdateProductInputModel ]
+
     }
 }
